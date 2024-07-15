94307956760a3d4cf823734839bb311111d1bb6b
disflyer <disflyer123@gmail.com>
2024-07-16 03:37:40 +0900

    :tada:

--- websit/apps/web/app/providers.tsx
+++ websit/apps/web/app/providers.tsx
@@ -1,12 +1,9 @@
-import { NextUIProvider } from '@nextui-org/react'
 import { TRPCReactProvider } from 'trpc/react'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <NextUIProvider>
-      <TRPCReactProvider>
-        {children}
-      </TRPCReactProvider>
-    </NextUIProvider>
+    <TRPCReactProvider>
+      {children}
+    </TRPCReactProvider>
   )
 }
